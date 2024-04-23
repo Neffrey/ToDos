a5da4ba1e2a44113d8ffd59de36e236967e63a4c
Neffrey <neffreyl@gmail.com>
2024-04-22 19:36:43 -0500

    Chore: Auth components

--- src/app/_components/auth-test.tsx
+++ src/app/_components/auth-test.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { useSession } from "next-auth/client";
+import { Button } from "~/components/ui/button";
 
 const AuthTest = () => {
-    
+  return (
+    <div>
+      <h1>Auth Test</h1>
+      <Button onClick={() => console.log("LOGIN CLICKED")}>Login</Button>
+    </div>
+  );
+};
+
+export default AuthTest;
